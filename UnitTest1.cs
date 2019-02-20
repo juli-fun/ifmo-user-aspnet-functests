553e294c29adc9cf03edc20d121fb4f5571eeabd
Julia Anikienko <juli-fun@Ramils-MacBook-Pro.local>
2019-02-20 23:39:06 +0100

    Добавлен функциональный тест формы авторизации

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ifmo_user_aspnet_functests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanAuthorize()
+        {
+            var driver = new ChromeDriver(@"/usr/local/bin");
+            driver.Navigate().GoToUrl("http://127.0.0.1:8080/");
+            var element1 = driver.FindElement(By.Name("_login_auth"));
+            element1.SendKeys("berezhkov");
+            var element2 = driver.FindElement(By.Name("_password_auth"));
+            element2.SendKeys("P@ssw0rd#123");
+            var element3 = driver.FindElement(By.Name("_reg_auth"));
+            element3.Click();
+
+            Assert.True(driver.FindElement(By.Id("_is_authorized")).Text == "Авторизован!");
+        }
+    }
+}
