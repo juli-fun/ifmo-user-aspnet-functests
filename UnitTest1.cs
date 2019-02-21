785de54f2eb456f075fb9779e64a9e08a58c8751
Julia Anikienko <juli-fun@Ramils-MacBook-Pro.local>
2019-02-21 01:10:57 +0100

    Добавлен функциональный тест изменения пользователя

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -20,7 +20,6 @@ namespace ifmo_user_aspnet_functests
             element3.Click();
 
             Assert.True(driver.FindElement(By.Id("is_authorized")).Text == "Авторизован!");
-            //driver.Close();
         }
 
         [Fact]
@@ -44,24 +43,37 @@ namespace ifmo_user_aspnet_functests
             }
 
             Assert.True(test_result);
-            //driver.Close();
         }
 
-        /*[Fact]
+        [Fact]
         public void CanEdit()
         {
+            var rnd = new Random();
+            int random = rnd.Next(1, 10000);
+            bool test_result = false;
+
             var driver = new ChromeDriver(@"/usr/local/bin");
             driver.Navigate().GoToUrl("http://127.0.0.1:8080/");
+
             var element1 = driver.FindElement(By.Name("_login"));
-            element1.SendKeys("testuser");
-            var element2 = driver.FindElement(By.Name("_password_auth"));
-            element2.SendKeys("P@ssw0rd#123");
-            var element3 = driver.FindElement(By.Name("_reg_auth"));
-            element3.Click();
+            element1.SendKeys("testuser" + random);
+            var element2 = driver.FindElement(By.Name("_reg_submit"));
+            element2.Click();
 
-            Assert.True(driver.FindElement(By.Id("_is_authorized")).Text == "Авторизован!");
+            var element1_1 = driver.FindElement(By.Name("_login"));
+            element1_1.SendKeys("testuser" + random);
+            var element3 = driver.FindElement(By.Name("_fname"));
+            element3.SendKeys("testedit" + random);
+            var element2_2 = driver.FindElement(By.Name("_reg_submit"));
+            element2_2.Click();
 
-            driver.Close();
-        }*/
+            var bodyTag = driver.FindElement(By.TagName("body"));
+            if (bodyTag.Text.Contains("testedit" + random))
+            {
+                test_result = true;
+            }
+
+            Assert.True(test_result);
+        }
     }
 }
